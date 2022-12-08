43a7f40e2cddff3ce99ecbfab3b735c38cd6eea2
fenderlopes@gmail.com <fenderlopes@gmail.com>
2022-12-08 16:36:38 -0300

    add regex code

--- src/App.jsx
+++ src/App.jsx
@@ -17,46 +17,45 @@ function App() {
       id: 1,
       name: "username",
       type: "text",
-      errorMessage: "Nome deve ter entre 3-16 caracteres, e não deve incluir qualquer caracter especial!",
+      errorMessage: "Nome deve ter entre 3-20 caracteres, e não deve incluir qualquer caracter especial!",
       placeholder: "Nome",
-      pattern: 'john',
+      pattern: '^[A-Za-z0-9]{3,20}$',
       required: true
 
     },
 
     {
-      id: 3,
+      id: 2,
       name: "email",
       type: "email",
       errorMessage: "Por favor, introduza o seu e-mail",
       placeholder: "Email",
-    
       required: true
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Date",
 
     },
     {
       id: 4,
-      name: "birthday",
-      type: "date",
-      placeholder: "Date",
+      name: "password",
+      type: "password",
+      errorMessage: "Sua senha deve conter pelo menos uma letra maiúscula, uma letra miúscula, um número e um caracter especial '#@%^_'",
+      placeholder: "Senha",
+      pattern: '^([A-Z][a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$',
       required: true
 
     },
     {
       id: 5,
-      name: "password",
-      type: "password",
-      errorMessage: "Por favor, insira sua senha",
-      placeholder: "Senha",
-      required: true
-
-    },
-    {
-      id: 6,
       name: "confirmPassword",
       type: "password",
       errorMessage: "Senhas não correspondem",
       placeholder: "Confirme a senha",
+      pattern: values.password,
       required: true
 
     },
@@ -64,12 +63,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    alert('Cadastrado com sucesso!')
+    console.log('values: ', values)
   }
   const onChange = (e) => {
     const { name, value } = e.target
     setValues((prevValue) => Object.assign({}, prevValue, { [name]: value }))
   }
-  console.log(values);
+ 
   return (
     <div className="flex justify-center flex-col items-center h-screen" >
       <div className="border border-gray-200 px-20 py-4 rounded">
