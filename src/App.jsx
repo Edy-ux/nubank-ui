ac6420e345902a858c9a3f435a81d779d8ffed0c
fenderlopes@gmail.com <fenderlopes@gmail.com>
2022-12-13 03:12:37 -0300

    change styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
     email: '',
     birthday: '',
     password: '',
-
     confirmPassword: ''
   })
   const inputs = [
@@ -70,26 +69,26 @@ function App() {
   }
   const onChange = (e) => {
     const { name, value } = e.target
-
     setValues((prevValue) => Object.assign({}, prevValue, { [name]: value }))
   }
 
 
   return (
-    <div className="bg-body bg-no-repeat w-screen bg-right-bottom md:bg-left bg-cover h-screen ">
-      <div className="flex justify-between  items-end md:items-center flex-col place-content-end lg:flex-row p-16 h-full">
-        <h1 className="text-white font-semibold w-96 mb-2 text-3xl md:text-5xl">Cartão de crédito com anuidade zero. Conta digital prática e sem custos.</h1>
-        <div className="px-8 py-8  rounded-2xl bg-white w-auto md:max-w-sm">
+    <div className="bg-body bg-no-repeat w-screen bg-cover h-screen">
+      <div className="flex justify-between items-center md:items-start lg:items-center flex-col lg:flex-row p-16  m-auto">
+        <blockquote>
+          <h1 className="text-white font-semibold w-96 mb-2 text-3xl md:text-5xl ">Cartão de crédito com anuidade zero. Conta digital prática e sem custos.</h1>
+        </blockquote>
+        <div className="px-8 py-8  rounded-2xl bg-white w-96">
           <p className="text-3xl font-medium">Com o Nubank, a resposta vem em menos de 1 minuto</p>
           <form onSubmit={handleSubmit}>
-            {inputs.map(input => (
-              <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange} label={input.placeholder}
-
+            {inputs.map((input, i)=> (
+              <FormInput key={input.id} {...input} value={values[i]} onChange={onChange}
               />
             ))}
-            <div className="text-center">
-              <button disabled className="w-full  bg-[#9d02d9] text-white text-[18px]  py-3 my-3 rounded-3xl ">Enviar</button>
-            </div>
+            
+              <button  className="w-full cursor bg-[#9d02d9] text-white text-[18px]  py-3 my-3 rounded-3xl text-semibold ">Cadastrar</button>
+            
           </form>
         </div>
 
